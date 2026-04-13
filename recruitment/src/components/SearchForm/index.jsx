075d6a5ea1675ac868ccb7b7d371a80dc1f71d44
Nguyễn Huỳnh Thành Danh <thanhdanh130425@gmail.com>
2026-04-13 10:39:08 +0700

    fecth api city

--- recruitment/src/components/SearchForm/index.jsx
+++ recruitment/src/components/SearchForm/index.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+import { getListCity } from "../../services/cityService";
+// import { useNavigate } from "react-router-dom";
+
 function SearchForm () {
+  // const navigate = useNavigate();
+  const [city, setCity] = useState();
+
+  useEffect(() => {
+    const fecthAPI = async () => {
+      const response = await getListCity();
+      setCity(response)
+    }
+    fecthAPI();
+  }, [])
+
+  console.log(city);
+
   return (
     <>
-      SearchForm
+      <h1>1000+ Jobs For Developers</h1>
     </>
   )
 }
